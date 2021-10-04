9e92c61febc42eeb1da89477f42647cb0e35cde7
Chun <caochun@gmail.com>
2021-10-04 17:59:54 +0800

    finished draft

--- com/anish/calabashbros/Creature.java
+++ com/anish/calabashbros/Creature.java
@@ -5,13 +5,11 @@ import java.awt.Color;
 public class Creature extends Thing {
 
     Creature(Color color, char glyph, World world) {
-        super(color, glyph,world);
+        super(color, glyph, world);
     }
 
     public void moveTo(int xPos, int yPos) {
-
         this.world.put(this, xPos, yPos);
-
     }
 
 }
